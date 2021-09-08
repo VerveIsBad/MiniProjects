2f6456d0232945d1f1ee66cb0e6fcf4ac3b4a5a9
Christopher Truluck <ctruluck12107@gmail.com>
2021-09-08 17:30:56 +0000

    Added comments so ik tf im doing lmao

--- Sockets_stuff/server.py
+++ Sockets_stuff/server.py
@@ -16,16 +16,26 @@ server.bind(ADDR)
 
 
 def handle_client(conn, addr):
+
+    # Shows client IP
     print(f"[NEW CONNECTION] {addr} connected.")
+
+    # Starts the loop; this happens after the thread starts this function.
     connected = True 
     while connected:
+        """
+        Message is received in bytes, so we decode this and convert it into the length of the message
+        Server: receives bytes message
+        Message: '(message length) 7 (Insert spaces for padding)'
+        """
         msg_length = conn.recv(HEADER).decode(FORMAT)
 
+        # Does message actually contain text / real message?
         if msg_length:
-
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
             
+            # Checks if the client has sent the, "Disconnect message"
             if msg == DISCCONECT_MESSAGE:
                 connected = False  
         
@@ -36,13 +46,17 @@ def handle_client(conn, addr):
 
 def start():
     server.listen()
+    # Prints the server IP
     print(f"[LISTENING] server is linstening on: {SERVER}")
+    
     while True:
         conn, addr = server.accept()
+        # directs a thread to the target function, and shows it's arguemnts.
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
+# Starts server
 print("[SERVER IS STARTING . . .]")
 start()
 
