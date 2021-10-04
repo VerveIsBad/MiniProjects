09ccc23dfbf488dc8ee4c641766cc39fe063e78f
Christopher we <ctruluck12107@gmail.com>
2021-10-04 12:45:41 +0000

    yes

--- c_stuff/main.cpp
+++ c_stuff/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 using namespace std;
 
+/*
+Basic statments
+----------------
+if (condition) {
+    // code 
+}
+
+while (condition) {
+    // executes until condition is false
+}
+
+for ( init; condition; increment ) {
+  // executes until condition is false
+}
+----------------
+
+Basic operaters
+----------------
+* = multiplaction
+/ = division 
+% = remainder after division
++ = addition
+- = subtraction
+----------------
++= 
+-=
+/=
+%=
+--------
+var++; prefix addition
+var--; preix subtraction
+--var; postfix subtraction 
+++var; postfix addition
+---------
+*/
 
 int main() {
-    cout << "test"; 
-    cout << "yes";
-    return 0;
+    cout << "hello\n";
 }
---
+++ c_stuff/run
@@ -0,0 +1,3 @@
+#!/bin/sh
+g++ main.cpp -o main
+./main
