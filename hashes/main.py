65b1b52685eef43628053d74880497e2a939ca60
Christopher we <ctruluck12107@gmail.com>
2021-10-06 16:44:46 +0000

    Added a large text file.

--- hashes/main.py
+++ hashes/main.py
@@ -1,18 +1,14 @@
 import hashlib
 target = "a532443f3e04a9e00295a8cd2a75e080" # target hash
-with open("sample.txt", 'rb') as f: # opens file in "Read bytes"
-    for line in f: # for line in file, it hash's the line 
-        # and compares its hexidecimanl value to the target.
-        result = hashlib.md5(line)
-        hexi = result.hexdigest()
 
-        if hexi.lower() == target:
+# Line 55 == Golduck
+
+with open("test.txt", "r") as f:
+    for line in f:
+        print(line)
+        if str(line) == "Golduck":
             print(True)
-            print(line.decode())
-        else:
-            print(False)
-    
-
+            break 
 
 
  
