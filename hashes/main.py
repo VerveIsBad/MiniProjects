55da56e799bc9303b6efb07e247f685b5ee88cad
Christopher we <ctruluck12107@gmail.com>
2021-10-06 18:44:03 +0000

    Added a working hash match program.

--- hashes/main.py
+++ hashes/main.py
@@ -1,15 +1,25 @@
 import hashlib
-target = "a532443f3e04a9e00295a8cd2a75e080" # target hash
+target = "999cae1e22fe69d89d6f56e3050f18cb" # target hash
+file = "sample.txt"
+def find_hash(file, target):
+    """
+    Given a target MD5 hash, and a file of words
+    It hash's every word in the file and compares it to the target hash.
+    """
+    with open(file, 'r') as f:
+        for line in f:
+            i = line.strip("\n")
+            n = i.lower()
+            x = n.encode()
+            test_case = hashlib.md5(x)
+            hexi = test_case.hexdigest()
+            if hexi == target:
+                print(f"Match found: {line}")
+                print(f"line = {hexi}, target = {target}")
+                return n
+                break
 
-# Line 55 == Golduck
 
-with open("test.txt", "r") as f:
-    for line in f:
-        print(line)
-        if str(line) == "Golduck":
-            print(True)
-            break 
+find_hash(file, target)
 
-
- 
 
